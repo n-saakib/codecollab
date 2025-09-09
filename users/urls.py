0d84eccb3950b3717bfff29eccc23a63e01d7388
Nazmus Sakib <nsakib800@gmail.com>
2025-09-09 13:04:07 +0600

    Updates signup page. Adds login and logout functionalities

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,18 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from users.views import LoginView, SignupView
+from users.views import SignupView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(template_name='users/logout.html'),
+        name='logout'
+    ),
 ]
