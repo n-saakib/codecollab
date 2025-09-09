46ca234d554ca92e85f5875e5e4b6d1acc7b541c
Nazmus Sakib <nsakib800@gmail.com>
2025-09-09 17:41:27 +0600

    Added viewsets and configured api endpoints

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
+from rest_framework.routers import DefaultRouter
 
-from projects.views import ProjectDashboardView
+from projects.views import ProjectDashboardView, ProjectViewSet, FileViewSet, FolderViewSet
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'folders', FolderViewSet, basename='folder')
+router.register(r'files', FileViewSet, basename='file')
 
 urlpatterns=[
     path('', RedirectView.as_view(pattern_name='dashboard', permanent=True)),
     path('dashboard/', ProjectDashboardView.as_view(), name='dashboard'),
+    path('api/', include(router.urls)),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from rest_framework import viewsets, permissions
 
-from projects.models import Project
+from projects.models import Project, Folder, File
+from projects.permissions import IsProjectMember
+from projects.serializers import ProjectSerializer, FolderSerializer, FileSerializer
 
 
 class ProjectDashboardView(LoginRequiredMixin, generic.ListView):
@@ -15,3 +18,36 @@ class ProjectDashboardView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows projects to be viewed or edited.
+    """
+    serializer_class = ProjectSerializer
+    permission_classes = (permissions.IsAuthenticated, IsProjectMember)
+
+    def get_queryset(self):
+        return Project.objects.filter(members=self.request.user)
+
+
+class FolderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Folders to be viewed or edited.
+    """
+    serializer_class = FolderSerializer
+    permission_classes = (permissions.IsAuthenticated, IsProjectMember)
+
+    def get_queryset(self):
+        return Folder.objects.filter(project__members=self.request.user)
+
+
+class FileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Files to be viewed or edited.
+    """
+    serializer_class = FileSerializer
+    permission_classes = (permissions.IsAuthenticated, IsProjectMember)
+
+    def get_queryset(self):
+        return File.objects.filter(project__members=self.request.user)
